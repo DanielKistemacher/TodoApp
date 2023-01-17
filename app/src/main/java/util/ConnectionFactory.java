62b4c21b6cc2985bc4cf05ee6d1375dbba579f8b
Daniel Kistemacher da Silva <106277555+DanielKistemacher@users.noreply.github.com>
2023-01-17 10:02:28 -0300

    update

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
-/**
- *
- * @author Daniel
- */
+//CLASSE UTILIZADA PARA CONEXÃO COM O BANCO DE DADOS LOCAL (LOCALHOST)
+
 public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+    
     
 }
